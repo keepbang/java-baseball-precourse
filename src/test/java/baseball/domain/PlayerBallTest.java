d3fbbf8eaafc20f7ac420bf24e42560ca5a97e07
keepbang <rlqud1125@naver.com>
2021-10-01 02:17:55 +0900

    test: 숫자 검증 테스트 분리

--- src/test/java/baseball/domain/PlayerBallTest.java
+++ src/test/java/baseball/domain/PlayerBallTest.java
@@ -3,35 +3,29 @@ package baseball.domain;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
-class BallTest {
-
+class PlayerBallTest {
     @ParameterizedTest
     @ValueSource(strings = {
-            "111","999","123","456","789","444"
+            "111","999","123","789"
     })
-    void 정규식_검증_성공(String number){
-        boolean matches = number.matches("^[1-9]{3}");
-
-        assertThat(matches).isTrue();
+    void 볼생성_성공(String input){
+        assertDoesNotThrow(() -> new PlayerBall(input));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {
-            "0",""," ","   ",
+            "0","000",""," ","   ",
             "1111","1000","9999",
             "+0","-99","_",
             "123abcd","abc","ab3",
             "123-12","11 ","1 9"," 12",
             " 342","123 "
     })
-    void 정규식_검증_실패(String number){
-        boolean matches = number.matches("^[1-9]{3}");
-
-        assertThat(matches).isFalse();
+    void 볼생성_실패(String input){
+        assertThatThrownBy(() -> new PlayerBall(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
-
 }
