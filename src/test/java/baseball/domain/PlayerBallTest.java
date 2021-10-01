8936edca43452d1eef944759184788e867946a2d
keepbang <rlqud1125@naver.com>
2021-10-01 21:57:47 +0900

    Test: 랜덤 숫자 생성 테스트코드 작성
    - 서로 다른 수로 이루어진 숫자인지 확인
    - PlayerBallTest에서 createPlayerBall로 객체생성

--- src/test/java/baseball/domain/PlayerBallTest.java
+++ src/test/java/baseball/domain/PlayerBallTest.java
@@ -3,6 +3,7 @@ package baseball.domain;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,7 +13,7 @@ class PlayerBallTest {
             "111","999","123","789"
     })
     void 볼생성_성공(String input){
-        assertDoesNotThrow(() -> new PlayerBall(input));
+        assertDoesNotThrow(() -> PlayerBall.createPlayerBall(input));
     }
 
     @ParameterizedTest
@@ -25,7 +26,32 @@ class PlayerBallTest {
             " 342","123 "
     })
     void 볼생성_실패(String input){
-        assertThatThrownBy(() -> new PlayerBall(input))
+        assertThatThrownBy(() -> PlayerBall.createPlayerBall(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "111","999","123","456","789","444"
+    })
+    void 정규식_검증_성공(String number){
+        boolean matches = number.matches("^[1-9]{3}");
+
+        assertThat(matches).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "0",""," ","   ",
+            "1111","1000","9999",
+            "+0","-99","_",
+            "123abcd","abc","ab3",
+            "123-12","11 ","1 9"," 12",
+            " 342","123 "
+    })
+    void 정규식_검증_실패(String number){
+        boolean matches = number.matches("^[1-9]{3}");
+
+        assertThat(matches).isFalse();
     }
 }
---
+++ src/test/java/baseball/utils/RandomGeneratorTest.java
@@ -0,0 +1,30 @@
+package baseball.utils;
+
+import baseball.domain.Ball;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static baseball.domain.PlayerBall.*;
+import static baseball.utils.RandomGenerator.getRandomPlayerBall;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class RandomGeneratorTest {
+
+    @Test
+    @DisplayName("서로 다른 수로 이루어진 숫자인지 확인")
+    void getRandomPlayerBallTest(){
+        Ball randomPlayerBall = getRandomPlayerBall();
+
+        List<Integer> numbers = randomPlayerBall.getNumbers();
+
+        Set<Integer> distinctSet = new HashSet<>(numbers);
+
+        assertThat(distinctSet.size()).isEqualTo(MAX_RANGE);
+    }
+
+}
