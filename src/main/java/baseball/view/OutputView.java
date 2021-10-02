1ffcc774a0e56115f048199840850b8decf7bbc5
keepbang <rlqud1125@naver.com>
2021-10-03 02:04:45 +0900

    test: 게임 출력에 대한 view 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,41 @@
+package baseball.view;
+
+import baseball.domain.Score;
+
+public class OutputView {
+    public static void enterNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static void inningResultOutput(Score score){
+        int strike = score.getStrike();
+        int ball = score.getBall();
+        String strikeResult = strikeFormatting(strike);
+        String ballResult = ballFormatting(ball);
+        String nothingResult = nothingFormatting(strike, ball);
+        System.out.println(strikeResult + ballResult + nothingResult);
+    }
+
+    private static String strikeFormatting(int strike){
+        if(strike > 0){
+            return strike + "스트라이크 ";
+        }
+        return "";
+    }
+
+    private static String ballFormatting(int ball){
+        if(ball > 0){
+            return ball + "볼 ";
+        }
+        return "";
+    }
+
+    private static String nothingFormatting(int strike, int ball){
+        if(strike == 0 && ball == 0){
+            return "낫싱";
+        }
+        return "";
+    }
+
+
+}
